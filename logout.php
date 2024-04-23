290e412afdd54ae13ba0c1473a034337d3c53d08
Nguyễn Trọng Tín <108884897+nguyentrongtinbku@users.noreply.github.com>
2024-04-23 22:43:37 +0700

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    if($_SESSION["uid"] == 0){
+        unset($_SESSION["uid"]);
+        unset($_SESSION["name"]);
+        header('Location: admin/login.php');
+    }
+    else{
+        unset($_SESSION["uid"]);
+        unset($_SESSION["name"]);
+        header('Location: index.php');
+    }
+?>
